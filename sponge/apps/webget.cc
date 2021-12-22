a9cd972d1c83a63090f2ab8d828c0b546fac6fb7
root <root@localhost.localdomain>
2021-12-22 17:04:29 +0800

    webget Done!

--- sponge/apps/webget.cc
+++ sponge/apps/webget.cc
@@ -6,20 +6,24 @@
 
 using namespace std;
 
+
 void get_URL(const string &host, const string &path) {
     // Your code here.
     TCPSocket socket;
     socket.connect(Address(host,"http"));
-
-    socket.write("GET"+path+"HTTP/1.1\r\n");
-    socket.write("Host: "+host+"\r\n");
-    socket.write("Connection: close\r\n\r\n");
-
+    socket.write("GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n");
+    //socket.write("GET"+path+"HTTP/1.1\r\n");
+    //socket.write("Host: "+host+"\r\n");
+    //socket.write("Connection: close\r\n\r\n");
+    socket.shutdown(SHUT_WR);
+    
     while(!socket.eof()){
 	    cout<< socket.read();
     }
+
+  
+    
     socket.close();
-    //socket.shutdown(SHUT_WR);
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -28,8 +32,8 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    //cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
